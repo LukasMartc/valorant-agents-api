de56b46025b035297785c7d9d4a202390a6f75c3
Lukas Martinez <lukasmartinezc@gmail.com>
2025-05-15 22:49:41 -0400

    Validación para crear agentes con express-validator

--- src/routes/agents.routes.js
+++ src/routes/agents.routes.js
@@ -1,9 +1,28 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { AgentsController } from '../controllers/AgentsController.js'
+import { handleInputErrors } from '../middleware/validation.js'
 
 const router = Router()
 
 router.get('/', AgentsController.getAgent)
-router.post('/', AgentsController.createAgent)
+router.post('/', 
+    body('name')
+        .toLowerCase()
+        .notEmpty().withMessage('El nombre del agente está vacio'),
+    body('role')
+        .notEmpty().withMessage('El rol del agente está vacio')
+        .toLowerCase()
+        .isIn(['duelista', 'controlador', 'iniciador', 'centinela'])
+        .withMessage('El rol del agente no es correcto'),
+    body('skills')
+        .toLowerCase()
+        .isArray({ min: 4, max: 4 }).withMessage('Debes incluir 4 habilidades'),
+    body('skills.*')
+        .isString()
+        .customSanitizer(value => value.toLowerCase())
+        .notEmpty().withMessage('La habilidad está vacia'),
+    handleInputErrors,
+    AgentsController.createAgent)
 
 export default router
