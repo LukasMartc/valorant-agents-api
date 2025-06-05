d4c4c5e55b14413bb18a2eb5bfd8008408a67f1c
Lukas Martinez <lukasmartinezc@gmail.com>
2025-06-05 14:37:20 -0400

    Validación con express validator del registro de usuarios listo

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation.js";
 import { registerUser } from "../controllers/auth.controller.js";
 
 const router = Router()
 
-router.post('/register', registerUser)
+router.post('/register',
+    body('name')
+      .notEmpty().withMessage('El nombre de usuario es obligatorio'),
+    body('email')
+      .notEmpty().withMessage('El email es obligatorio')
+      .isEmail().withMessage('Email inválido')
+      .normalizeEmail(),
+    body('password')
+      .notEmpty().withMessage('La contraseña es obligatoria')
+      .isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+    handleInputErrors,
+    registerUser)
 
 export default router
